2fc27daad22e759bb0f6127a3d4820d90a4bb86f
Joey T <thechickenbane@gmail.com>
2017-12-31 00:44:44 -0800

    Add Service and Test
    Also add a .gitignore and all the files IntelliJ automagically makes for you =/

--- src/main/kotlin/com/googlejobapp/reactivespringboot/Application.kt
+++ src/main/kotlin/com/googlejobapp/reactivespringboot/Application.kt
@@ -5,6 +5,14 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class ReactiveApplication {
@@ -13,7 +21,22 @@ class ReactiveApplication {
     fun wha() = ApplicationRunner {
         log.info("oh hai")
     }
+
+    /*
+    @Bean
+    fun router() = RouterFunction<ServerResponse> {
+        return RouterFunctions.route(RequestPredicates.GET("/hai") {
+            req -> ok().body
+        }) }
+    }
+    */
 }
+
+@Service
+class HaiService {
+    fun generic(): Mono<String> = Mono.just("ugh")
+}
+
 
 fun main(args: Array<String>) {
     runApplication<ReactiveApplication>(*args)
--- src/test/kotlin/com/googlejobapp/reactivespringboot/HaiServiceTest.kt
+++ src/test/kotlin/com/googlejobapp/reactivespringboot/HaiServiceTest.kt
@@ -1,5 +1,20 @@
 package com.googlejobapp.reactivespringboot
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
-internal class HaiServiceTest
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HaiServiceTest {
+    @Autowired lateinit var service: HaiService
+
+    @Test
+    fun testGeneric() {
+        val generic = service.generic().block()
+        assertEquals("response", "ugh", generic)
+    }
+}
